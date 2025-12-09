959bbfa9ffbc21094c89e1e4f5c66378b05af3c7
Ron Religioso <111487700+kaynewhite@users.noreply.github.com>
2025-12-09 15:53:13 +0800

    fixed running cond

--- app.py
+++ app.py
@@ -1267,4 +1267,5 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()
         create_default_users()
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port, debug=False)
